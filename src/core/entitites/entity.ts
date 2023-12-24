4f4a5d00d2a9835856416b0bd5511c8c00afdbb8
izael gomes <izaelgomes127@gmail.com>
2023-12-24 13:22:10 -0300

    refactor: class responsible to generate ID

--- src/core/entitites/entity.ts
+++ src/core/entitites/entity.ts
@@ -1,16 +1,17 @@
-import { randomUUID } from "node:crypto"
+import { randomUUID } from "node:crypto";
+import { UniqueEntityId } from "./unique-entity-id";
 
 export class Entity<TProps> {
-    private _id: string
-    protected props: TProps
-    
+  private _id: UniqueEntityId;
 
-    get id() {
-        return this._id
-    }
-    
-    constructor(props:TProps, id?: string){
-        this.props = props
-        this._id = id ?? randomUUID()
-    }
+  protected props: TProps;
+
+  get id() {
+    return this._id;
+  }
+
+  constructor(props: TProps, id?: string) {
+    this.props = props;
+    this._id = new UniqueEntityId(id);
+  }
 }
